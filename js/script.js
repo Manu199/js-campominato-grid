3b8ff66953769d86b9ee5c06ac9171fbdcd79fc8
Manu199 <manu199gh@gmail.com>
2023-09-19 15:50:00 +0200

    [fix] RandomNumber

--- js/script.js
+++ js/script.js
@@ -8,47 +8,66 @@
 */
 
 const container = document.querySelector('.container');
-    const numbersBlacklist = [];
+const numbersBlacklist = [];
+const playBtn = document.querySelector( '#play')
+const levelSelect = document.querySelector('#griglia')
 
+
+
+let cellNumbers;
+const levels = [100, 81, 49]
+
+ playBtn.addEventListener('click', play);
+
+  function play() {
+    console.log('PLAY');
+    cellNumbers = levels[levelSelect.value];
     reset();
+    init();
+  }
 
-    function init() {
-      for (let i = 1; i <= 100; i++) {
-        const randomNumber = getUniqueRandomNumber(1, 100);
-        const square = createSquare(randomNumber);
 
-        square.addEventListener('click', function () {
-          console.log(this._squareID);
-          this.classList.toggle('clicked');
-          this.classList.toggle(oddEven(this._squareID));
+  function generatePlayGround()
 
-        });
 
-        container.append(square);
-      }
-    }
 
-    function oddEven(id) {
-      if (id % 2) return 'odd';
-      else return 'even';
-    }
-    
 
-    function createSquare(index) {
 
-    /*
-      1. crea l'elemento 
-      2. gli aggiungoi la classe square
-      3. restituisco l'elemento creato
 
-    */
+    // function init() {
+    //   for (let i = 1; i <= 100; i++) {
+    //     // const randomNumber = getUniqueRandomNumber(1, 100);
+    //     // const square = createSquare(randomNumber);
+    //     const square = createSquare(i);
 
-      const newSquare = document.createElement('div');
-      newSquare.className = 'square';
-      newSquare._squareID = index;
-      newSquare.innerHTML = `<span>${index}</span>`;
-      return newSquare;
-    }
+
+    //     square.addEventListener('click', function () {
+    //       console.log(this._squareID);
+    //       this.classList.toggle('clicked');
+    //       // this.classList.toggle(oddEven(this._squareID));
+
+    //     });
+
+    //     container.append(square);
+    //   }
+    // }
+
+
+    // function createSquare(index) {
+
+    // /*
+    //   1. crea l'elemento 
+    //   2. gli aggiungoi la classe square
+    //   3. restituisco l'elemento creato
+
+    // */
+
+    //   // const newSquare = document.createElement('div');
+    //   // newSquare.className = 'square';
+    //   // newSquare._squareID = index;
+    //   // // newSquare.innerHTML = `<span>${index}</span>`;
+    //   // return newSquare;
+    // }
 
     function reset() {
       container.innerHTML = '';
@@ -67,21 +86,4 @@ const container = document.querySelector('.container');
       return btn;
     }
 
-    function getUniqueRandomNumber(min, max) {
-      let randomNumber;
-      let isValidNumber = false;
-
-      while (!isValidNumber) {
-        randomNumber = getRandomNumber(min, max);
-        if (!numbersBlacklist.includes(randomNumber)) {
-          numbersBlacklist.push(randomNumber);
-          isValidNumber = true;
-        }
-      }
-
-      return randomNumber;
-    }
-
-    function getRandomNumber(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  
